94d047a0d16ed568751b07b6bbde13e3f89c5a99
Martin <matovu.synergy@gmail.com>
2021-02-08 13:22:10 +0300

    Update main script file to load the stylesheet

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 	<head>
 		<meta charset="<?php bloginfo('charset'); ?>">
 		<title><?php bloginfo('name'); ?></title>
+		<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
 	</head>
 	<body></body>
 </html>
