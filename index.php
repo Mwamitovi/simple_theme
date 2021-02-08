c51208a2daba8449e69c57c303daedbe24d0261d
Martin <matovu.synergy@gmail.com>
2021-02-08 14:27:45 +0300

    Add boilerpalte code for WP 'loop'

--- index.php
+++ index.php
@@ -10,6 +10,15 @@
 		<header>
 			<h1><?php bloginfo('name'); ?></h1>
 			<small><?php bloginfo('description'); ?></small>
+			<div class="main">
+				<!-- The loop -->
+				<?php if(have_posts()): ?>
+					post found
+				<?php else: ?>
+					<!-- wpautop() takes double line breaks and makes them into paragraphs -->
+					<?php echo wpautop('Sorry, No posts were found'); ?>
+				<?php endif; ?>
+			</div>
 		</header>
 	</body>
 </html>
