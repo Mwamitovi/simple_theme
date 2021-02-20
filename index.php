fe1539875754dcfa62a6d4a26676b36eaf1a34ff
Martin <matovu.synergy@gmail.com>
2021-02-20 14:22:30 +0300

    Create a "single post" script file

--- index.php
+++ index.php
@@ -9,29 +9,37 @@
 		<!-- the_post() tells WP to move to the next post -->
 		<?php while( have_posts() ): the_post(); ?>
 
-			<h3>
-				<a href="<?php the_permalink(); ?>">
-					<?php the_title(); ?>					
+			<article class="post">
+				
+				<h3>
+					<a href="<?php the_permalink(); ?>">
+						<?php the_title(); ?>					
+					</a>
+				</h3>
+
+				<div class="meta">
+
+					<!-- Created by matovu on February 8, 2021 -->
+					<!-- Created by <?php /** the_author(); ?> on <?php the_date(); **/ ?><br/> -->
+
+					<!-- Created by matovu on Monday 8th of February 2021 09:14:01 AM -->
+					<!-- Created by <?php /** the_author(); ?> on <?php the_date('l jS \of F Y h:i:s A'); **/ ?><br/> -->
+
+					<!-- Created by matovu on 9:14 am -->
+					<!-- Created by <?php /** the_author(); ?> on <?php the_time(); **/ ?><br/> -->
+
+					<!-- Created by matovu on February 8, 2021 9:14:am -->
+					Created by <?php the_author(); ?> on <?php the_time('F j, Y g:i:a'); ?>
+
+				</div>
+
+				<?php the_content(); ?>
+
+				<a class="button" href="<?php the_permalink(); ?>">
+					Read more
 				</a>
-			</h3>
 
-			<div class="meta">
-
-				<!-- Created by matovu on February 8, 2021 -->
-				<!-- Created by <?php /** the_author(); ?> on <?php the_date(); **/ ?><br/> -->
-
-				<!-- Created by matovu on Monday 8th of February 2021 09:14:01 AM -->
-				<!-- Created by <?php /** the_author(); ?> on <?php the_date('l jS \of F Y h:i:s A'); **/ ?><br/> -->
-
-				<!-- Created by matovu on 9:14 am -->
-				<!-- Created by <?php /** the_author(); ?> on <?php the_time(); **/ ?><br/> -->
-
-				<!-- Created by matovu on February 8, 2021 9:14:am -->
-				Created by <?php the_author(); ?> on <?php the_time('F j, Y g:i:a'); ?>
-
-			</div>
-			
-			<?php the_content(); ?>
+			</article>
 
 		<?php endwhile; ?>
 
